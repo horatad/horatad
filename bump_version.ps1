ea18a1491e9a0db3138e3e248819a80ff39a9d34
horatad <uchujaro5@yahoo.com>
2026-05-19 20:40:31 +0700

    commit message

--- bump_version.ps1
+++ bump_version.ps1
@@ -4,14 +4,19 @@ $parts = $old.Split('.')
 $newV = $parts[0] + '.' + $parts[1] + '.' + ([int]$parts[2] + 1)
 Write-Host "  $old -> $newV"
 
-Set-Content 'version.json' "{`"v`":`"$newV`"}" -Encoding UTF8
+$utf8NoBom = [System.Text.UTF8Encoding]::new($false)
 
-$s = Get-Content 'script.js' -Raw
+# version.json
+[System.IO.File]::WriteAllText((Resolve-Path 'version.json'), "{`"v`":`"$newV`"}", $utf8NoBom)
+
+# script.js
+$s = [System.IO.File]::ReadAllText((Resolve-Path 'script.js'), $utf8NoBom)
 $s = $s -replace "const APP_VERSION='[0-9]+\.[0-9]+\.[0-9]+'", "const APP_VERSION='$newV'"
-Set-Content 'script.js' $s -Encoding UTF8 -NoNewline
+[System.IO.File]::WriteAllText((Resolve-Path 'script.js'), $s, $utf8NoBom)
 
-$w = Get-Content 'sw.js' -Raw
+# sw.js
+$w = [System.IO.File]::ReadAllText((Resolve-Path 'sw.js'), $utf8NoBom)
 $w = $w -replace "const CACHE_NAME='horatad-v[0-9]+\.[0-9]+\.[0-9]+'", "const CACHE_NAME='horatad-v$newV'"
-Set-Content 'sw.js' $w -Encoding UTF8 -NoNewline
+[System.IO.File]::WriteAllText((Resolve-Path 'sw.js'), $w, $utf8NoBom)
 
 Write-Host "[2/3] Bumped: version.json + script.js + sw.js -> $newV"
